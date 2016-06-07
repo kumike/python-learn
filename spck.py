6f599c9fae2a8fd6c84ba10706c2a1c1332ca3c0
kumike <michailkubrak@gmail.com>
2016-06-08 02:14:56 +0300

    color output

--- spck.py
+++ spck.py
@@ -4,10 +4,11 @@ dia=int(input('\033[96mЧисло комбинаций: \033[0m'))
 speed=int(input('\033[96mСкорость перебора:\033[0m '))
 hour=3600
 minute=60
+day=24
 val=dia//(speed*hour)
 if val<=0:
     val=dia//(speed*minute)
-    print('Минут',val)
+    print('\033[1;32mМинут',val,'\033[1;m')
 if val>0 and val<=24: 
-    print('Часов',val)
-if val>=24:val=val//24;print('  ',val,'Cуток') # укороченый синтаксис if, простые конструкции можно писать коротко
+    print('\033[1;32mЧасов',val,'\033[1;m')
+if val>=24:val=val//day;print('  \033[32m',val,'Cуток\033[1;m') # укороченый синтаксис if, простые конструкции можно писать коротко
